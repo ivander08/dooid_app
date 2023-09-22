6020e2fcb28af5323f5fb7643cbe2e8299760ffc
ivander08 <ivanderseah08@gmail.com>
2023-09-22 19:06:11 +0700

    Initial Commit
    Finished Loading and Onboarding Screen.

---
+++ lib/screens/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Text('REGISTER SCREEN'),
+    );
+  }
+}
